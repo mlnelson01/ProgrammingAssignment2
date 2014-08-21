50f6efe6de49429f2b15f7f585e788704dc03caf
mlnelson01 <mlnelson01@comcast.net>
2014-08-21 07:39:47 -0400

    Submission for R Assignment 2
    This file contains the functions needed to complete Assignment 2. Use
    this file for peer review.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a cahe of a matrix inverse. 
+##They assume the input matrix x is invertible.
 
-## Write a short comment describing this function
+## This first function makes the vector that will set and cache the matrix.
+##The input argument is x, and invertible matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## Set local variables
+  iofx <- matrix(NA)
+  
+  ##Define set function
+  set <- function(y) {
+    x <<- y
+    iofx <<- matrix(NA)
+  }
+  ##Get function to get the matrix x
+  get <- function() x
+  ##Define setinverse function to create inverse of x
+  setinverse <- function(iofx) i <<- iofx
+  ## Define getinverse function, which simply returns inverse of x is it exists
+  getinverse <- function() i
+  ##combine functions into list and return them
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+##This function returns the inverse of the matrix.
+##The input argument is the list created by makeCacheMatrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  browser()
+## Return a matrix that is the inverse of 'x'
+  iofx <- x$getinverse()
+  if(!is.na(iofx[1,1])) {
+    message("getting cached data")
+    return(iofx)
+  }
+  data <- x$get()
+  iofx <- solve(data)
+  x$setinverse(iofx)
+  iofx
 }
